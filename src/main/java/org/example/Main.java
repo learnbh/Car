9dbcb15817a84ed268ed8599ade61cf63b1f8e82
learnjavabh <learnmsbh@outlook.de>
2025-04-09 17:17:41 +0200

    added Scanner to choose a vehicle and change the velocity

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,29 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Um zwischen Auto oder Fahrrad zu wählen drücke: \n 1 für Auto \n 2 für Fahrrad");
+        String vehicle = scanner.nextLine();
+        System.out.println("Anhalten = 0\n oder Geschwindigkeit erhöhen zahl>0 eingeben");
+        int accelerate = scanner.nextInt();
+        if (vehicle.equals("1")) {
+            Car car = new Car("01", "Trabi");
+            if(accelerate == 0)
+                car.brake();
+            else
+                car.accelerate(accelerate);
+        }
+        else{
+            Bicycle bicycle = new Bicycle("01", "Mountainbike");
+            if(accelerate == 0)
+                bicycle.brake();
+            else
+                bicycle.accelerate(accelerate);
+
+        }
+
     }
 }
